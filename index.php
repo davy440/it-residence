717f6e914b4c68d4949adab1236ca3e62d6dcee2
Divjot <divjotdavy@gmail.com>
2024-09-20 12:27:48 -0400

    pagination and other layout fixes

--- index.php
+++ index.php
@@ -49,8 +49,6 @@ if ( $sidebar_align == 'left' ) {
 			<?php
             the_posts_pagination(  array(
 				'class'					=>	'itre-pagination',
-				'before_page_number'	=>	'<span>',
-				'after_page_number'		=>	'</span>',
 				'prev_text'				=> '<span class="arrow-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></span>',
 				'next_text'				=> '<span class="arrow-next"><i class="fa fa-angle-right" aria-hidden="true"></i></span></i>'
 			) );
